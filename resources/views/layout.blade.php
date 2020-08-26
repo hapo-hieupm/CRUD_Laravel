22db3fddcd31dba5e1a054025306f31fc27bae74
hapo-hieupm <68210143+hapo-hieupm@users.noreply.github.com>
2020-08-26 14:13:43 +0700

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,18 +4,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD Test</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('css/bootstrap.min.css') }}" type="text/css" rel="stylesheet" />
-    <link href="{{ asset('css/dataTables.bootstrap.min.css') }}" type="text/css" rel="stylesheet" />
-    <link href="{{ asset('css/bootstrap-custom.css') }}" type="text/css" rel="stylesheet" />
 </head>
 <body>
     <div class="container">
         @yield('main')
     </div>
     <script src="{{ asset('js/app.js') }}" type="text/js"></script>
-    <script type="text/javascript" src="{{ asset('js/jquery.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/jquery.dataTables.min.js') }}"></script>
-    <script type="text/javascript" src="{{ asset('js/dataTables.bootstrap.min.js') }}"></script>
 </body>
 </html>
